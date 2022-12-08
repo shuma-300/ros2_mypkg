1395f663888778748f961530f9ad21bcf918fb79
Shuma <s21c1014zh@s.chibakoudai.jp>
2022-12-09 08:32:56 +0900

    Add a command

--- install/mypkg/lib/python3.10/site-packages/mypkg/talker.py
+++ install/mypkg/lib/python3.10/site-packages/mypkg/talker.py
@@ -2,18 +2,23 @@ import rclpy
 from rclpy.node import Node
 from std_msgs.msg import Int16
 
-rclpy.init()
-node = Node("talker")
-pub = node.create_publisher(Int16, "countup", 10)
-n = 0
+class Talker():          #ヘッダの下にTalkerというクラスを作成
+    def __init__(self, node):  
+        self.pub = node.create_publisher(Int16, "countup", 10)
+        self.n = 0
+        node.create_timer(0.5, self.cd)
+        
+    def cb(self):      #インデントをあげてselfを引数に
+        msg = Int16()
+        msg.data = self.n     #talker -> self
+        self.pub.publish(msg) #talker -> self
+        self.n += 1           #talker -> self
 
-def cd():
-    global n
-    msg = Int16()
-    msg.data = n
-    pub.publish(msg)
-    n += 1
+def main():
+    rclpy.init()
+    node = Node("talker")
+    talker = Talker(node)
+    rclpy.spin(node)
 
-node.create_timer(0.5, cd)
-rclpy.spin(node)
-
+if __name__ == '__main__':
+    main()
